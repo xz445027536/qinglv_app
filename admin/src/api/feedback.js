75fd4827b79801befcc0dd91f880b7b6d5736f6c
肖卓 <445027536@qq.com>
2020-05-18 19:26:04 +0800

    修改项目结构

--- admin/src/api/feedback.js
+++
@@ -1,9 +0,0 @@
-import request from '../utils/request'
-
-export function getFeedbackList(params) {
-  return request({
-    url: '/suggestionFeedback/getFeedbackList',
-    method: 'post',
-    data: params
-  })
-}
